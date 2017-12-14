8696de17a188e0f3d092ad2b28f6b0347734f48a
Bastet1982 <oriele.nieto@gmail.com>
2017-12-14 14:17:19 -0300

    foodmap

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,58 +5,16 @@ $(document).ready(function(){
       $('#splash').fadeOut(500);     // id splash del html, donde la funcion fadeOut desaparecerá a los 5 seg la pantalla de inicio
    }, 2000);
 });
-
-	$(document).ready(function(){    // enlace con index html  REVISAR!!!
-    $("#nav a").each(function(){
-      var href = $(this).attr("href");
-    $(this).attr({ href: "#"});
-    $(this).click(function(){
-        $("#show").load(href);
-      });
-   });
+   
+     $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myList").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });*/
 });
 
-	var busqueda = $('#busqueda'),
-		titulo = $('ul li h3');
 
-	$(titulo).each(function(){
-		var li = $(this);
-
-	//si presionamos la tecla
-	$(busqueda).keyup(function(){
-
-	//cambiamos a minusculas
-	this.value = this.value.toLowerCase();
-	//
-	var clase = $('.search i');
-	if($(busqueda).val() != ''){
-	$(clase).attr('class', 'fa fa-times');
-	}else{
-	$(clase).attr('class', 'fa fa-search');
-	}
-	if($(clase).hasClass('fa fa-times')){
-	$(clase).click(function(){
-	//borramos el contenido del input
-	$(busqueda).val('');
-	//mostramos todas las listas
-	$(li).parent().show();
-	//volvemos a añadir la clase para mostrar la lupa
-	$(clase).attr('class', 'fa fa-search');
-	});
-	}
-	//ocultamos toda la lista
-	$(li).parent().hide();
-	//valor del h3
-	var txt = $(this).val();
-	//si hay coincidencias en la búsqueda cambiando a minusculas
-	if($(li).text().toLowerCase().indexOf(txt) > -1){
-	//mostramos las listas que coincidan
-	$(li).parent().show();
-	}
-
-   });
-});
-});
 
 
 
